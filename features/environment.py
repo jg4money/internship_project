24fb4f42615b2d6dad3d9570282e8a88f6db5f7d
Jacob Grable <grabledjacob@gmail.com>
2023-10-23 15:40:47 -0700

    browserstack complete

--- features/environment.py
+++ features/environment.py
@@ -1,38 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from app.application import Application
+
 
 def browser_init(context):
     """
     :param context: Behave context
     """
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
+    #driver_path = ChromeDriverManager().install()
+    #service = Service(driver_path)
+    #context.driver = webdriver.Chrome(service=service)
 
     ### OTHER BROWSERS
     #service = Service(executable_path= "/Users/jacobgrable/QA/python-selenium-automation/geckodriver")
     #context.driver = webdriver.Firefox(service=service)
     #context.driver = webdriver.Safari()
 
+    ### BROWSERSTACK ###
+    #Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user = 'jacobgrable_02vm1G'
+    bs_key = 'qcMffBqrPwXHx1H7JwoH'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+          'os': 'Windows',
+          'osVersion': '11',
+          'browserName': 'Chrome',
+          'sessionName': 'Reelly.io'
+     }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
     context.driver.maximize_window()
 
     ### HEADLESS MODE ####
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(
-         options=options,
-         service=service
-    )
+    #options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')
+    #service = Service(driver_path)
+    #context.driver = webdriver.Chrome(
+         #options=options,
+         #service=service
+    #)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
 
     context.app = Application(context.driver)
 
-def before_scenario(context, scenario):
+def before_scenario(context, scenario,):
     print('\nStarted scenario: ', scenario.name)
     browser_init(context)
 
@@ -46,6 +64,6 @@ def after_step(context, step):
         print('\nStep failed: ', step)
 
 
-def after_scenario(context, feature):
-    context.driver.delete_all_cookies()
-    context.driver.quit()
+    def after_scenario(context, feature):
+        context.driver.delete_all_cookies()
+        context.driver.quit()
